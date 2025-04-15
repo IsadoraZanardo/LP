e8e7a8b9c5b56dbeda09ee8d401e28fca7cb0736
Isadora Zanardo <isaszanardo@outlook.com>
2025-04-14 22:49:21 -0300

    i

--- Listas de Exercícios/LP_Lista_04/ex6-4/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/ex6-4/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite o menor número: ");
+        int n1 = in.nextInt();
+
+        System.out.println("Digite o maior número: ");
+        int n2 = in.nextInt();
+
+        for(int i=n1+1; i<n2; i++) {
+            if (i>0) {
+                System.out.println(i);
+            }
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_04/ex8-4/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/ex8-4/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite seu núemro: ");
+        num = in.nextInt();
+
+        if(num<1)
+            System.out.println("Não é primo");
+
+        else {}
+
     }
 }
